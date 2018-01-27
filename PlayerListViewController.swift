f233f7f0330377f5a1bff7b1a12f691776f7a17a
Ava Henkemeyer <henkemeyer@davids-macbook-pro-2.local>
2018-01-27 13:24:42 -0800

    First Commit

---
+++ PlayerListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PlayerListViewController.swift
+//  AboveAndBelowScorer
+//
+//  Created by David Henkemeyer on 1/27/18.
+//  Copyright © 2018 David Henkemeyer. All rights reserved.
+//
+
+import UIKit
+
+class PlayerListViewController: UIViewController {
+
+    @IBOutlet weak var player1Score: UILabel!
+    @IBOutlet weak var player2Score: UILabel!
+    @IBOutlet weak var player3Score: UILabel!
+    @IBOutlet weak var player4Score: UILabel!
+    @IBOutlet weak var player1Button: UIButton!
+    @IBOutlet weak var player2Button: UIButton!
+    @IBOutlet weak var player3Button: UIButton!
+    @IBOutlet weak var player4Button: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
